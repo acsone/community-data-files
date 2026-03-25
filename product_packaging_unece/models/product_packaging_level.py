ff15a1ee9734118c72e318eaf52e94a40c67e5e9
Souheil Bejaoui <souheil.bejaoui@acsone.eu>
2026-03-25 11:02:07 +0100

    [IMP] product_packaging_unece: cache UNECE code mapping
    add an ormcache based helper to map UNECE codes to packaging level

--- product_packaging_unece/models/product_packaging_level.py
+++ product_packaging_unece/models/product_packaging_level.py
@@ -1,7 +1,7 @@
 # Copyright 2026 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.tools import ormcache
 
 
 class ProductPackagingLevel(models.Model):
@@ -16,3 +16,26 @@ class ProductPackagingLevel(models.Model):
         "nomenclature of the United Nations Economic "
         "Commission for Europe (UNECE), DataElement Rec 21)",
     )
+
+    @api.model
+    @ormcache("unece_code")
+    def _get_packaging_level_ids_by_unece_code(self, unece_code):
+        if not unece_code or not isinstance(unece_code, str):
+            return []
+        return self.search([("unece_type_ids.code", "=", unece_code)]).ids
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        self._get_packaging_level_ids_by_unece_code.clear_cache(self)
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._get_packaging_level_ids_by_unece_code.clear_cache(self)
+        return res
+
+    def unlink(self):
+        res = super().unlink()
+        self._get_packaging_level_ids_by_unece_code.clear_cache(self)
+        return res
